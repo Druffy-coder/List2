ea7cd64c25e7c14daa8433c4342faf93d0dc58e1
Dima <SemenihinDmit@yandex.ru>
2020-10-15 15:37:57 +0300

    added List and subClass node with fields and constructors

---
+++ Derevo/List.java
@@ -0,0 +1,20 @@
+package com.max.idea;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class List<T> implements Collection<T> {
+    class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
